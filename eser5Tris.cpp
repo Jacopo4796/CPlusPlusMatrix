050184ab543f0138384a2e14b3534f5a01078af3
Jacopo <jcp.federico@gmail.com>
2023-03-16 15:28:32 +0100

    eser5 Tris completo e funzionante

--- eser5Tris.cpp
+++ eser5Tris.cpp
@@ -1,65 +1,165 @@
 #include <iostream>
-
 using namespace std;
 
-// 0 1 2
-// 3 4 5
-// 6 7 8
+const char VUOTO = '-';
 
 int mossaPensata(char *tab)
 {
-    
+    int risultato = -1;
+    int combinazioni[8][3] = {
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+        {0, 4, 8},
+        {2, 4, 6}};
+
+    // prima controllo se il giocatore può vincere
+    // se il giocatore può vincere io lo blocco
+
+    for (int i = 0; i < 8; i++) // due X su 0 e 1
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][1]];
+        char c = tab[combinazioni[i][2]];
+        bool condizione = a == b && c == VUOTO && a == 'X';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][2];
+        }
+    }
+
+    for (int i = 0; i < 8; i++) // due X su 0 e 2
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][2]];
+        char c = tab[combinazioni[i][1]];
+        bool condizione = a == b && c == VUOTO && a == 'X';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][1];
+        }
+    }
+
+    for (int i = 0; i < 8; i++) // due X su 1 e 2
+    {
+        char a = tab[combinazioni[i][1]];
+        char b = tab[combinazioni[i][2]];
+        char c = tab[combinazioni[i][0]];
+        bool condizione = a == b && c == VUOTO && a == 'X';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][0];
+        }
+    }
+
+    // ora controllo se il computer può vincere
+    // se ci sono due O metto il terzo
+
+    for (int i = 0; i < 8; i++) // due O su 0 e 1
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][1]];
+        char c = tab[combinazioni[i][2]];
+        bool condizione = a == b && c == VUOTO && a == 'O';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][2];
+        }
+    }
+
+    for (int i = 0; i < 8; i++) // due O su 0 e 2
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][2]];
+        char c = tab[combinazioni[i][1]];
+        bool condizione = a == b && c == VUOTO && a == 'O';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][1];
+        }
+    }
+
+    for (int i = 0; i < 8; i++) // due O su 1 e 2
+    {
+        char a = tab[combinazioni[i][1]];
+        char b = tab[combinazioni[i][2]];
+        char c = tab[combinazioni[i][0]];
+        bool condizione = a == b && c == VUOTO && a == 'O';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][0];
+        }
+    }
+
+    if (risultato == -1)
+    {
+        risultato = random() % 9;
+    }
+
+    return risultato;
 }
 
-//  1 per la vittoria
-//  0 per continuare la partita
-// -1 per il pareggio
+/*
+isWinner restituisce:
+1 per la vittoria
+0 per continuare la partita
+-1 per il pareggio
+*/
 int isWinner(char *arr)
 {
-    for (int i = 0; i < 7; i += 3) // Righe (indice max riga 6)
+    int combinazioni[8][3] = {
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+        {0, 4, 8},
+        {2, 4, 6}};
+
+    for (int i = 0; i < 8; i++)
     {
-        if (arr[i] != '-')
+        int a = arr[combinazioni[i][0]];
+        int b = arr[combinazioni[i][1]];
+        int c = arr[combinazioni[i][2]];
+
+        if (a != VUOTO && a == b && b == c)
         {
-            if ((arr[i] == arr[i + 1]) && (arr[i + 1] == arr[i + 2]))
-            {
-                return 1; // Hai vinto
-            }
+            return 1;
         }
     }
-    for (int i = 0; i < 3; i++) // Colonne (indice max colonna 2)
+
+    for (int i = 0; i < 9; i++)
     {
-        if (arr[i] != '-')
+        if (arr[i] == VUOTO)
         {
-            if ((arr[i] == arr[i + 3]) && (arr[i] == arr[i + 6]))
-            {
-                return 1; // Hai vinto
-            }
+            return 0; // c'è ancora spazio
         }
     }
-    if (((arr[0] != '-') && (arr[0] == arr[4]) && (arr[0] == arr[8])) || ((arr[2] != '-') && (arr[2] == arr[4]) && (arr[2] == arr[6]))) // Diagonali
-    {
-        return 1; // Hai vinto
-    }
-    for (int i = 0; i < 9; i++) // Scorro tutto l'array
-    {
-        if (arr[i] == '-') // Se ci sono spazi "vuoti"
-        {
-            return 0; // Continua a giocare
-        }
-    }
-    return -1; // Se non viene realizzata nessuna delle condizioni precedenti allora c'è un pareggio
+
+    return -1;
 }
 
-void stampa(char *a)
+void stampa(char *tabella)
 {
     for (int i = 0; i < 9; i++)
     {
-        cout << a[i] << " ";
-        if (i == 2 || i == 5 || i == 8)
+        cout << tabella[i] << " ";
+        if (i == 2 || i == 5 || i == 8) // alla fine di ogni riga (3x3) vai a capo
         {
             cout << endl;
         }
     }
+
     cout << endl;
     cout << endl;
     cout << endl;
@@ -67,45 +167,45 @@ void stampa(char *a)
 
 int main()
 {
-    char vuoto = '-';
     // inizializzo la tabella
     char tabella[9];
     for (int i = 0; i < 9; i++)
     {
-        tabella[i] = vuoto;
+        tabella[i] = VUOTO;
     }
     stampa(tabella);
+
     bool xIsNext = true; // gestisce il turno
     while (true)         // i turni del gioco
     {
         int mossa;
-        if (!xIsNext) // Se sono giocatore 2
+
+        if (!xIsNext) // COMPUTER
         {
             mossa = mossaPensata(tabella);
-            while (tabella[mossa] != vuoto || (!(0 <= mossa && mossa < 9)))
-            {
-                mossa = rand() % 10;
-            }
         }
-        else
+        else // UTENTE
         {
             cout << "Giocatore " << (xIsNext ? 'X' : 'O') << " inserisci la tua mossa (1-9) ";
             cin >> mossa;
-            mossa--;
-            while (tabella[mossa] != vuoto || (!(0 <= mossa && mossa < 9))) // controllo 2 condizioni (valido e vuoto)
+            mossa--; // per capirci ;)
+
+            while (tabella[mossa] != VUOTO || (!(0 <= mossa && mossa < 9))) // controllo 2 condizioni (valido e VUOTO)
             {
                 cout << "Non puoi inserire qui" << endl;
                 cout << "Giocatore " << (xIsNext ? 'X' : 'O') << " inserisci la tua mossa (1-9) ";
                 cin >> mossa;
-                mossa--;
+                mossa--; // per capirci ;)
             }
         }
         // se sono arrivato qui è solo perchè l'utente ha inserito un numero corretto
         xIsNext ? tabella[mossa] = 'X' : tabella[mossa] = 'O'; // se è vero metti X altrimenti metti O
+
         stampa(tabella);
+
         if (isWinner(tabella) == 1)
         {
-            cout << "Hai vinto " << (xIsNext ? 'X' : 'O') << endl;
+            cout << "Ha vinto " << (xIsNext ? 'X' : 'O') << endl;
             break;
         }
         else if (isWinner(tabella) == -1)
@@ -114,7 +214,7 @@ int main()
             // reset del gioco
             for (int i = 0; i < 9; i++)
             {
-                tabella[i] = vuoto;
+                tabella[i] = VUOTO;
             }
             xIsNext = true;
             stampa(tabella);
